e3a4368a1574c56af0247f305935829dd3b1529e
vighakos <vighakos@turr.hu>
2022-01-27 14:11:32 +0100

    ball XD

---
+++ labda/app.js
@@ -0,0 +1,14 @@
+let ball = document.querySelector('#ball');
+let ballsize = 54;
+let speed = 1;
+let screenX = window.innerWidth;
+let screenY = window.innerHeight;
+let ballX = Math.round(Math.random()*screenX);
+let ballY = 0;
+
+ball.style.top = ballY + 'px';
+ball.style.left = ballX + 'px';
+
+function moveBall(){
+    
+}
