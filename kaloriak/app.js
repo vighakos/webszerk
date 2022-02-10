523edf1021f56d124a12c3ca70831739c6c58775
vighakos <vighakos@turr.hu>
2022-02-10 14:07:13 +0100

    kaloria

---
+++ kaloriak/app.js
@@ -0,0 +1,14 @@
+let input = document.getElementsByTagName('input');
+let sportok = [
+    {nev:'futas', kcal: '680'},
+    {nev:'foci', kcal: '550'},
+    {nev:'bringa', kcal: '480'},
+    {nev:'hegymasz', kcal: '420'},
+    {nev:'lovaglas', kcal: '370'},
+    {nev:'turazas', kcal: '360'},
+    {nev:'kajakozas', kcal: '340'},
+    {nev:'sulyzos', kcal: '320'},
+    {nev:'pingpong', kcal: '270'},
+    {nev:'kutyaseta', kcal: '200'},
+];
+
