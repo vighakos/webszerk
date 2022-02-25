2fbbaa6e11feaf8e0868da6c8f9c0daef655060e
vighakos <vighakos@turr.hu>
2022-02-25 11:35:26 +0100

    aaa

--- dobokocka/app.js
+++ dobokocka/app.js
@@ -10,6 +10,160 @@ console.log("4. feladat: Ennyiszer volt hatos dobás: " + hatos_ossz())
 console.log("5. feladat: " + harom_hatos() + " három hatos dobás")
 console.log("6. feladat: " + azonos_szam())
 console.log("7. feladat: " + kulonbozo_szamok())
+console.log("8. feladat: A dobások " + szazalek().toFixed(2) + "%-a volt 2-es, 4-es, vagy 6-os")
+console.log("9. feladat: " + keterteke() + " olyan dobás, ahol a legnagyobb számot mutató kocka a másik két kocka értékének összege.")
+console.log("9. feladat: " + keterteke() + " olyan dobás, ahol a legnagyobb számot mutató kocka a másik két kocka értékének összege.")
+console.log("10. feladat: Ezt a számot mutatták be legkevesebbszer: " + legkevesebb());
+console.log("11. feladat: Ez az összeg többször is előfordult: " + osszeg_elofordul());
+
+function osszeg_elofordul() {
+    for(i = 0; i < a.length; i++){
+        
+    }
+}
+
+function legkevesebb() {
+    let egy = 0;
+    let ketto = 0;
+    let harom = 0;
+    let negy = 0;
+    let ot = 0;
+    let hat = 0;
+    for(i = 0; i < a.length; i++){
+        switch (a[i]) {
+            case 1:
+                egy++;
+                break;
+            
+            case 2:
+                ketto++;
+                break;
+            
+            case 3:
+                harom++;
+                break;
+            
+            case 4:
+                negy++;
+                break;
+            
+            case 5:
+                ot++;
+                break;
+            
+            case 6:
+                hat++;
+                break;
+            
+            default:
+                break;
+        }
+        switch (b[i]) {
+            case 1:
+                egy++;
+                break;
+            
+            case 2:
+                ketto++;
+                break;
+            
+            case 3:
+                harom++;
+                break;
+            
+            case 4:
+                negy++;
+                break;
+            
+            case 5:
+                ot++;
+                break;
+            
+            case 6:
+                hat++;
+                break;
+            
+            default:
+                break;
+        }
+        switch (c[i]) {
+            case 1:
+                egy++;
+                break;
+            
+            case 2:
+                ketto++;
+                break;
+            
+            case 3:
+                harom++;
+                break;
+            
+            case 4:
+                negy++;
+                break;
+            
+            case 5:
+                ot++;
+                break;
+            
+            case 6:
+                hat++;
+                break;
+            
+            default:
+                break;
+        }
+    }
+    let tomb = [];
+    tomb.push(egy, ketto, harom, negy, ot, hat)
+    let min = a[0];
+    for(i = 0; i < tomb.length; i++){
+        if (min < tomb[i]) {
+            min = i + 1
+        }
+    }
+    return min;
+}
+
+function keterteke() {
+    let van = false;
+    for(i = 0; i < a.length; i++){
+        if (a[i] > b[i] && a[i] > c[i]) {
+            if (a[i] == b[i] + c[i]) {
+                van = true;
+                break;
+            }
+        }else if (b[i] > a[i] && b[i] > c[i]) {
+            if (b[i] == a[i] + c[i]) {
+                van = true;
+                break;
+            }
+        }else if (c[i] > a[i] && c[i] > b[i]) {
+            if (c[i] == a[i] + b[i]) {
+                van = true;
+                break;
+            }
+        }
+    }
+    return van ? "Volt" : "Nem volt";
+}
+
+function szazalek() {
+    let szamlalo = 0;
+    for(i = 0; i < a.length; i++){
+        if (a[i] % 2 == 0) { 
+            szamlalo++;
+        }
+        if (b[i] % 2 == 0) {
+            szamlalo++;
+        }
+        if (c[i] % 2 == 0) {
+            szamlalo++;
+        }
+    }
+    return (szamlalo / 60) * 100;
+}
 
 function kulonbozo_szamok() {
     tomb = [];
