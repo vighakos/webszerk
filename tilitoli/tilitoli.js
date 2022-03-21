2c3f4b77fc980eb24d5b03e13df6125e0715b04e
vighakos <vighakos@turr.hu>
2022-03-21 09:39:03 +0100

    a

---
+++ tilitoli/tilitoli.js
@@ -0,0 +1,16 @@
+let gameTable = document.querySelector('#gameTable'),
+    newGameBtn = document.querySelector('#newGameBtn'),
+    stepCountBox = document.querySelector('stepCountBox'),
+    timeBox = document.querySelector('timeBox'),
+    game = false,
+    stepCount = 0,
+    mperc = 0,
+    perc = 0,
+    ora = 0,
+    elements = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function drawTable() {
+    for(i = 0; i < ; i++){
+    
+    }
+}
