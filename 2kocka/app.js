e9ad87beacb1dc8c5c1635dab44f98b293067c69
vighakos <vighakos@turr.hu>
2022-03-21 17:28:38 +0100

    2kocka2

--- 2kocka/app.js
+++ 2kocka/app.js
@@ -5,49 +5,29 @@ let dobasok = document.querySelector('#dobasok')
 let stat = document.querySelector('#stat')
 let a = []
 let b = []
-let stat_tomb = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-let stat_szazalek = []
 let counter = 0
+let stat_szamok = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 feltolt()
-statisztika_feltolt()
+stat_kiir()
 
 dobas.addEventListener('click', function () {
     feltolt()
+    stat_kiir()
 })
 
-function statisztika() {
-    let elozodobas = kiszamol()
-    let max = 100
-    let osszesdobas = a.length + b.length
-    
-    for(i = 0; i < stat_tomb.length; i++){
-        if (stat_tomb[i] != 0) {
-            stat_szazalek[i] = stat_tomb[i]
-            console.log(stat_szazalek[i]);
-        }
+function stat_kiir() {
+    let szoveg = ""
+    for(i = 0; i < stat_szamok.length; i++){
+        szoveg += `<div class='szazalek' style='width: ` + szazalek(i).toFixed(0) + `%;'>` + (i+2) + `: ` + szazalek(i).toFixed(0) + `%</div>`
     }
+    stat.innerHTML = szoveg
 }
 
-function statisztika_feltolt() {
-    for(i = 0; i < stat_tomb.length; i++){
-        if (stat_tomb[i] != 0) {
-            stat.innerHTML += `<p id='p_` + (i + 2) + `'>` + (i) +  `: ` + stat_szazalek[i] +`</p>`
-        } else {
-            stat.innerHTML += `<p id='p_` + (i + 2) + `'>` + (i) +  `: 0</p>`
-        }
-    }
-    let p_2 = document.getElementById('p_2')
-    let p_3 = document.getElementById('p_3')
-    let p_4 = document.getElementById('p_4')
-    let p_5 = document.getElementById('p_5')
-    let p_6 = document.getElementById('p_6')
-    let p_7 = document.getElementById('p_7')
-    let p_8 = document.getElementById('p_8')
-    let p_9 = document.getElementById('p_9')
-    let p_10 = document.getElementById('p_10')
-    let p_11 = document.getElementById('p_11')
-    let p_12 = document.getElementById('p_12')
+function szazalek(x) {
+    let szam = 0;
+    if (stat_szamok[x] != 0) szam = ((stat_szamok[x] / counter) * 100)
+    return szam
 }
 
 function kiszamol() {
@@ -56,11 +36,8 @@ function kiszamol() {
 
 function feltolt() {
     tarolo.innerHTML = ketdobas()
-    eredmeny.innerHTML = kiszamol()
-    counter++
+    eredmeny.innerHTML = "Eredmény: " + kiszamol()
     dobasok.innerHTML = "Dobások száma: " + counter
-    statisztika()
-    
 }
 
 function ketdobas() {
@@ -75,11 +52,21 @@ function ketdobas() {
     dobas += (`<img src='img/` + random + `.PNG' class='kocka'></img>`)
     b.push(random)
 
-    for(i = 0; i < stat_tomb.length; i++){
-        if (kiszamol() == i + 2) {
-            stat_tomb[i + 2] = kiszamol()
-            console.log(stat_tomb[i + 2])
-        }
+    switch (kiszamol()) {
+        case 2: {stat_szamok[0]++; break;}
+        case 3: {stat_szamok[1]++; break;}
+        case 4: {stat_szamok[2]++; break;}
+        case 5: {stat_szamok[3]++; break;}
+        case 6: {stat_szamok[4]++; break;}
+        case 7: {stat_szamok[5]++; break;}
+        case 8: {stat_szamok[6]++; break;}
+        case 9: {stat_szamok[7]++; break;}
+        case 10: {stat_szamok[8]++; break;}
+        case 11: {stat_szamok[9]++; break;}
+        case 12: {stat_szamok[10]++; break;}
     }
+
+    counter++
+
     return dobas
 }
