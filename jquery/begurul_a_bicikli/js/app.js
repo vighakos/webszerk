07f83095be2bc729b957e21dead484e1574c8808
vighakos <vighakos@turr.hu>
2022-05-30 09:23:50 +0200

    víz és kerékpár

--- jquery/begurul_a_bicikli/js/app.js
+++ jquery/begurul_a_bicikli/js/app.js
@@ -5,6 +5,11 @@ let jarmuvek = [],
     elvitel = form.elvitel,
     visszahoz = form.visszahoz
 
+$(document).ready(function() {
+    $("#kolszam").text(`Összes kölcsönzés: ${jarmuvek.length}`)
+    $("#penz").text(`Bevétel: ${calculatePenz()}Ft`)
+})
+
 if (adatok = localStorage.getItem('jarmuvek')) {
     jarmuvek = JSON.parse(adatok)
 }
@@ -14,7 +19,7 @@ $("#felveszBtn").on("click", () => {
         azon.value == "" ||
         elvitel.value == "" ||
         visszahoz.value == "" || 
-        !verifyDate()) 
+        !verifyDate(elvitel.value, visszahoz.value)) 
     {
         alert('Minden mező kitöltése kötelező!')
     } else {
@@ -30,10 +35,27 @@ $("#felveszBtn").on("click", () => {
     }
 })
 
-function verifyDate() {
-    try {
+function verifyDate(el, vissza) {
+    let el_ora = parseInt(el.split(":")[0]),
+        el_perc = parseInt(el.split(":")[1])
+
+    let vissza_ora = parseInt(vissza.split(":")[0]),
+        vissza_perc = parseInt(vissza.split(":")[1])
+
+    if ((el_ora >= 0 && el_ora < 24 && el_perc >= 0 && el_perc < 60) || 
+        (vissza_ora >= 0 && vissza_ora < 24 && vissza_perc >= 0 && vissza_perc < 60))
+    {
+        return true
+    } else return false
+}
+
+function calculatePenz() {
+    let ossz = 0
+    jarmuvek.forEach(jarmu => {
+        let el_percben = parseInt(jarmu.elvitel.split(":")[0]) * 60 + parseInt(jarmu.elvitel.split(":")[1]),
+            vissza_percben = parseInt(jarmu.visszahoz.split(":")[0]) * 60 + parseInt(jarmu.visszahoz.split(":")[1])
         
-    } catch (error) {
-        
-    }
+        ossz += Math.ceil((vissza_percben - el_percben) / 30) * 2400
+    })
+    return ossz
 }
