cbc532797c35e3eae2335584b3bda6b3164eb702
vighakos <vighakos@turr.hu>
2022-06-02 12:29:36 +0200

    .

---
+++ jquery/kpo/js/app.js
@@ -0,0 +1,9 @@
+if (opponent.move == rock) {
+    player.move = papper    // :)
+}
+if (opponent.move == papper) {
+    player.move = skizzor    // :)
+}
+if (opponent.move == skizzor) {
+    player.move = rock    // :)
+}
