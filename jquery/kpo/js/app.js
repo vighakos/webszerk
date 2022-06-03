f32bd44706c9436ddb4cffb91fe3ba43bad7cc4b
vighakos <vighakos@turr.hu>
2022-06-03 09:39:27 +0200

    .

--- jquery/kpo/js/app.js
+++ jquery/kpo/js/app.js
@@ -1,9 +1,64 @@
-if (opponent.move == rock) {
-    player.move = papper    // :)
+let ko_valaszt = document.querySelector('#ko'),
+    papir_valaszt = document.querySelector('#papir'),
+    ollo_valaszt = document.querySelector('#ollo'),
+    player = document.querySelector('#player'),
+    pc = document.querySelector('#pc'),
+    acceptBtn = document.querySelector('#acceptBtn')
+
+
+ko_valaszt.addEventListener('click', () => {
+    player.classList.add(valaszt(ko_valaszt.id))
+})
+papir_valaszt.addEventListener('click', () => {
+    player.classList.add(valaszt(papir_valaszt.id))
+})
+ollo_valaszt.addEventListener('click', () => {
+    player.classList.add(valaszt(ollo_valaszt.id))
+})
+
+acceptBtn.addEventListener('click', () => {
+    pcMove()
+    
+})
+
+function valaszt(val) {
+    if (val == 'ko') {
+        player.classList = ""
+        player.classList.add('img')
+        player.classList.add('ko')
+    }
+    if (val == 'papir') {
+        player.classList = ""
+        player.classList.add('img')
+        player.classList.add('papir')
+    }
+    if (val == 'ollo') {
+        player.classList = ""
+        player.classList.add('img')
+        player.classList.add('ollo')
+    }
 }
-if (opponent.move == papper) {
-    player.move = skizzor    // :)
-}
-if (opponent.move == skizzor) {
-    player.move = rock    // :)
+
+function pcMove() {
+    let move = Math.floor(Math.random() * 4)
+
+    switch (move) {
+        case 1:
+            pc.classList = ""
+            pc.classList.add('img')
+            pc.classList.add('ko')
+            break;
+    
+        case 2:
+            pc.classList = ""
+            pc.classList.add('img')
+            pc.classList.add('papir')
+            break;
+    
+        case 3:
+            pc.classList = ""
+            pc.classList.add('img')
+            pc.classList.add('ollo')
+            break;
+    }
 }
